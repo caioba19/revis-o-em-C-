dd2477bfa91eaf407f0161b29f3175445794aa65
Caio Vinicius Santos da Costa Leite <157418334+caioba19@users.noreply.github.com>
2026-04-07 11:37:23 -0300

    pra salvar

--- programa.c
+++ programa.c
@@ -17,4 +17,71 @@ int main() {
 }
 
 //2
+#include <stdio.h>
 
+int main() {
+
+    int a = 5, b = 15;
+    int *p1, *p2;
+
+    p1 = &a;
+    p2 = &b;
+
+    printf("Antes:\n");
+    printf("A: %d \t\t B: %d\n", a, b);
+
+    *p1 = 100;
+    *p2 = 200;
+
+    printf("Depois:\n");
+    printf("A: %d \t\t B: %d\n", a, b);
+
+    return 0;
+}
+
+//3 
+#include <stdio.h>
+
+int main() {
+
+    float v1, v2, soma;
+    float *p1, *p2;
+
+    p1 = &v1;
+    p2 = &v2;
+
+    printf("Informe o primeiro valor: ");
+    scanf("%f", &v1);
+
+    printf("Informe o segundo valor: ");
+    scanf("%f", &v2);
+
+    soma = *p1 + *p2;
+
+    printf("Soma = %.2f\n", soma);
+
+    return 0;
+}
+
+//4
+#include <stdio.h>
+
+void eleva_ao_quadrado(int *n);
+
+int main() { 
+    int valor = 6;
+
+    printf("Valor antes da função: %d\n", valor);
+    
+    eleva_ao_quadrado(&valor);
+
+    printf("Valor depois da função: %d\n", valor);
+
+    return 0;
+}
+
+void eleva_ao_quadrado(int *n){
+    *n = (*n) * (*n);
+}
+
+//5
