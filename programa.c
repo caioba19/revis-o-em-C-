aa61739582db9d30ccd8107e2dbb9df99d023d01
Caio Vinicius Santos da Costa Leite <157418334+caioba19@users.noreply.github.com>
2026-04-07 11:44:41 -0300

    =pp

--- programa.c
+++ programa.c
@@ -85,3 +85,27 @@ void eleva_ao_quadrado(int *n){
 }
 
 //5
+#include <stdio.h>
+
+void troca(int *a, int *b);
+
+int main() { 
+    
+    int x = 10, y = 20;
+
+    printf("Antes da troca: x = %d, y = %d\n", x, y);
+
+    troca(&x, &y);
+
+    printf("Depois da troca: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
+void troca(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+//6
